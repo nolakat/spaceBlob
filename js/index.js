d5c4fadcc16bd564a582cf156c0d4e6595f284e1
Sullivan Nolan <nolakat@gmail.com>
2019-02-22 17:36:37 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 import 'gsap';
 
-TweenMax.staggerFrom(".blink", 1, {delay: 0.15, opacity: 0, repeat: -1, yoyo: true}, 0.2);
+// TweenMax.staggerFrom(".blink", 1, {delay: 0.15, opacity: 0, repeat: -1, yoyo: true}, 0.2);
 
-TweenMax.to(".bigstar", 1, { repeat: -1, yoyo: true});
-TweenMax.to(".bigstar-circle", 1, {scale: 1.2, repeat: -1, yoyo:true, ease:Back.easeOut, transformOrigin: "center center"
-});
+// TweenMax.to(".bigstar", 1, { repeat: -1, yoyo: true});
+// TweenMax.to(".bigstar-circle", 1, {scale: 1.2, repeat: -1, yoyo:true, ease:Back.easeOut, transformOrigin: "center center"
+// });
 
-TweenMax.to("#comet", 4, {x:1600, y: 1000, scale: .25, repeat: -1, repeatDelay: 10, ease: Expo.easeOut});
+// TweenMax.to("#comet", 4, {x:1600, y: 1000, scale: .25, repeat: -1, repeatDelay: 10, ease: Expo.easeOut});
 
-		 var scene = document.getElementById('scene');
-		 var parallaxInstance = new Parallax(scene, {
-			invertX: true,
-			invertY: true,
-			limitX: 26,
-			limitY: 5
-		 });
+// var scene = document.getElementById('scene');
+// var parallaxInstance = new Parallax(scene, {
+// 	invertX: true,
+// 	invertY: true,
+// 	limitX: 26,
+// 	limitY: 5
+// });
